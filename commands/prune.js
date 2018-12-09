5d9b4ea6acda3049a5290a0e6f8150ff10679f45
Jeckup907 <31732500+Jeckup907@users.noreply.github.com>
2018-12-09 13:25:20 +0100

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -5,17 +5,21 @@ module.exports = {
 	guildOnly: true,
 	execute(message, args) {
 		const amount = parseInt(args[0]) + 1;
+		if (message.member.roles.some(r=>['Moderator furry', 'The Master of the Furries' ].includes(r.name))) {
+			if (isNaN(amount)) {
+				return message.reply('that doesn\'t seem to be a valid number.');
+			}
+			else if (amount <= 1 || amount > 100) {
+				return message.reply('you need to input a number between 1 and 99.');
+			}
 
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
+			message.channel.bulkDelete(amount, true).catch(err => {
+				console.error(err);
+				message.channel.send('there was an error trying to prune messages in this channel!');
+			});
 		}
-		else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
+		else {
+			return message.reply('It doesn\'t seem like you have the right to use this command');
 		}
-
-		message.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
-			message.channel.send('there was an error trying to prune messages in this channel!');
-		});
 	},
 };
