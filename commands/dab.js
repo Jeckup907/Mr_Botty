e0638f8c6560285e8e029d73d6993ebc69545e0e
Jeckup907 <31732500+Jeckup907@users.noreply.github.com>
2018-12-30 00:03:33 +0100

    dab

--- commands/dab.js
+++ commands/dab.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'dab',
 	description: 'dab on them haters',
 	execute(message) {
-		message.channel.send('DAB', {
+		message.channel.send({
 			// gets a random link from the Links.json file and then posts it
 			file: dabLinks[Math.floor(Math.random() * dabLinks.length)],
 		});
