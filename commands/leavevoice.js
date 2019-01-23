c4deecaea273e19536bfff5ef06069c8ef15f0c9
Jeckup907 <31732500+Jeckup907@users.noreply.github.com>
2019-01-23 18:03:09 +0100

    updated leave voice command

--- commands/leavevoice.js
+++ commands/leavevoice.js
@@ -4,7 +4,13 @@ module.exports = {
 	description: 'if the bot is in the same voice channel, it makes them leave',
 	guildOnly: true,
 	execute(message) {
+		if (message.channel.type !== 'text') return;
 		const { voiceChannel } = message.member;
-		voiceChannel.leave();
+		if (!voiceChannel) {
+			return message.reply('The bot is not in a voice channel');
+		}
+		voiceChannel.leave().then(msg => {
+			msg.delete(10000);
+		});
 	},
 };
