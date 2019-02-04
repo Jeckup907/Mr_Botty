b2e482640bff5767b3633880431ae1ff1d4c2104
Jeckup907 <31732500+Jeckup907@users.noreply.github.com>
2019-02-04 09:40:35 +0100

    updated the server command

--- commands/server.js
+++ commands/server.js
@@ -1,8 +1,20 @@
+const Discord = require('discord.js');
 module.exports = {
 	name: 'server',
 	description: 'Display info about this server.',
 	guildOnly: true,
 	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+
+		const exampleEmbed = new Discord.RichEmbed()
+			.setColor('#0099ff')
+			.setTitle('$Server')
+			.setThumbnail(message.guild.iconURL)
+			.addField('Server name', message.guild.name)
+			.addField('The owner', message.guild.owner)
+			.addField('Total members', message.guild.memberCount)
+			.addField('Roles', message.guild.roles)
+			.addField('Time of creation', message.guild.createdAt);
+
+		message.channel.send(exampleEmbed);
 	},
 };
