1afc393926668e55833de865dc1f61b4d6ea0d7e
Jeckup907 <31732500+Jeckup907@users.noreply.github.com>
2019-02-06 15:21:13 +0100

    remove unessecary code

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ client.once('ready', () => {
 });
 // When the bot have seen a message being sent in one of the channels it has access too, that starts with the prefix and is not a bot user it will continue with the code inside the brackets
 client.on('message', message => {
-	if((message.content.toLowerCase().includes('furrygang')) || (message.content.toLowerCase().includes('furry gang'))) {
-		message.react('526113207539204097');
-		console.log('Reacted to a message');
-	}
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const commandName = args.shift().toLowerCase();
@@ -37,7 +33,6 @@ client.on('message', message => {
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 	// if the message isn't a command, return to top
 	if (!command) return;
-	// post's a message in the log of who used which command in which channel and server
 	if (command.guildOnly && message.channel.type !== 'text') {
 		return message.reply('I can\'t execute that command inside DMs!');
 	}
@@ -51,7 +46,6 @@ client.on('message', message => {
 	if(command && message.guild && !command.prune) {
 		message.delete(3000);
 	}
-	console.log(command);
 	if (!cooldowns.has(command.name)) {
 		cooldowns.set(command.name, new Discord.Collection());
 	}
